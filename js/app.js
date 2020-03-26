151f27ccef328485af4efdf3c2d7db0cacd9a02c
do1thingwell <do1thingwell@Macs-MBP.net.fpt>
2020-03-27 00:27:36 +0700

    share

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@ document.body.insertBefore(toNodes(`
       <div class="navbar-item">
         <div class="field is-grouped">
           <p class="control">
-            <div class="fb-share-button"  data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Flambdademy.github.io%2Fcovid19-selfcheck%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Chia sẻ</a>
-            </div>
+            <div class="fb-share-button" data-href="http://chungtaygopsuc.com/" data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fchungtaygopsuc.com&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Chia sẻ</a>
+          </div>
           </p>
          
         </div>
