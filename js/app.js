4e99bbb657bbe877d667c262d3bd91808a382bf5
do1thingwell <do1thingwell@Macs-MBP.net.fpt>
2020-03-26 11:51:51 +0700

    menu

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ document.body.insertBefore(toNodes(`
       <div class="navbar-item">
         <div class="field is-grouped">
           <p class="control">
-            <div class="fb-share-button" data-href="https://lambdademy.github.io/covid19-selfcheck/" data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Flambdademy.github.io%2Fcovid19-selfcheck%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Chia sẻ</a>
+            <div class="fb-share-button"  data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Flambdademy.github.io%2Fcovid19-selfcheck%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Chia sẻ</a>
             </div>
           </p>
          
